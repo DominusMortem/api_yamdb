c2dce9d8b0087d68eb5f5fa9d5af502e4f56ba07
Zinnurov Ramil <ramil.zinnurov@infotecs.ru>
2022-04-25 23:46:21 +0500

    Создал модель Жанры

--- api_yamdb/titles/models.py
+++ api_yamdb/titles/models.py
@@ -3,7 +3,15 @@ from django.db import models
 
 class Categories(models.Model):
     name = models.CharField(max_length=256)
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(unique=True, max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Genres(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(unique=True, max_length=50)
 
     def __str__(self):
         return self.name
