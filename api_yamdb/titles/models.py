7f5f6202622247f97370d5d4dcdd43e97349a51b
Zinnurov Ramil <ramil.zinnurov@infotecs.ru>
2022-04-25 23:52:17 +0500

    Создал модель Произведения

--- api_yamdb/titles/models.py
+++ api_yamdb/titles/models.py
@@ -15,3 +15,14 @@ class Genres(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Titles(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.TextField(blank=False)
+    year = models.IntegerField()
+    category = models.ForeignKey(Categories, on_delete=models.CASCADE)
+    genre = models.ForeignKey(Genres, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
