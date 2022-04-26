401fba8797b6b87be60cf9d00472a140dce69c71
Zinnurov Ramil <ramil.zinnurov@infotecs.ru>
2022-04-26 22:52:08 +0500

    Убрал лишние модели для фичи Категории

--- api_yamdb/titles/models.py
+++ api_yamdb/titles/models.py
@@ -1,28 +1,9 @@
 from django.db import models
 
 
-class Categories(models.Model):
+class Category(models.Model):
     name = models.CharField(max_length=256)
     slug = models.SlugField(unique=True, max_length=50)
-
-    def __str__(self):
-        return self.name
-
-
-class Genres(models.Model):
-    name = models.CharField(max_length=256)
-    slug = models.SlugField(unique=True, max_length=50)
-
-    def __str__(self):
-        return self.name
-
-
-class Titles(models.Model):
-    name = models.CharField(max_length=256)
-    description = models.TextField(blank=False)
-    year = models.IntegerField()
-    category = models.ForeignKey(Categories, on_delete=models.CASCADE)
-    genre = models.ForeignKey(Genres, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
