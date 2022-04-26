a81895be27acf57051537ef39aab73ec53742fb5
Алексей Уколов <ulex07@gmail.com>
2022-04-26 23:08:00 +0400

    Добавил модель Comment

--- api_yamdb/titles/models.py
+++ api_yamdb/titles/models.py
@@ -17,3 +17,13 @@ class Review(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField(
         'Дата добавления', auto_now_add=True, db_index=True)
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='comments')
+    review = models.ForeignKey(
+        Review, on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField()
+    pub_date = models.DateTimeField(
+        'Дата добавления', auto_now_add=True, db_index=True)
