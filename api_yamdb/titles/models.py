5a0295435f2b56b2ee3574067432c228f9d0733c
Алексей Уколов <ulex07@gmail.com>
2022-04-26 22:49:11 +0400

    Добавил модели

--- api_yamdb/titles/models.py
+++ api_yamdb/titles/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Title(models.Model):
+    name = models.TextField()
+
+
+class Review(models.Model):
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='comments')
+    title = models.ForeignKey(
+        Title, on_delete=models.CASCADE, related_name='comments')
+    score = models.FloatField()
+    text = models.TextField()
+    pub_date = models.DateTimeField(
+        'Дата добавления', auto_now_add=True, db_index=True)
