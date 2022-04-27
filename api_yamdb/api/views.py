7e41e7ef7cdf2806698a33b4e918bdd47c5a3a9e
Zinnurov Ramil <ramil.zinnurov@infotecs.ru>
2022-04-27 22:18:06 +0500

    Добавил модель, сериализатор, урл и вьюсет для жанров

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -2,12 +2,19 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 from rest_framework import filters
 
-from .serializers import CategorySerializer
-from titles.models import Category
+from .serializers import CategorySerializer, GenreSerializer
+from titles.models import Category, Genre
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter)
+    search_fields = ('name',)
+
+
+class GenresViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
     filter_backends = (DjangoFilterBackend, filters.SearchFilter)
     search_fields = ('name',)
