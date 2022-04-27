0d5192077a1c3a9ee069b67b4f7f5522efc1c8d3
Zinnurov Ramil <ramil.zinnurov@infotecs.ru>
2022-04-27 10:49:58 +0500

    Добавил фильтрация и пагинацию для категории

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,4 +1,7 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
+from rest_framework import filters
+
 from .serializers import CategorySerializer
 from titles.models import Category
 
@@ -6,3 +9,5 @@ from titles.models import Category
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter)
+    search_fields = ('name',)
