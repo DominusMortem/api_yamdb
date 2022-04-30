892b32536a5b7a68c5e1761d96c55d83e44507f4
Серафим Попов <grafff93@gmail.com>
2022-04-30 17:02:44 +0300

    Вручную слил все файлы)

--- api_yamdb/api/mixins.py
+++ api_yamdb/api/mixins.py
@@ -4,3 +4,10 @@ from rest_framework import mixins, viewsets
 class CreateViewSet(mixins.CreateModelMixin,
                     viewsets.GenericViewSet):
     pass
+
+
+class ListCreateDeleteViewSet(mixins.ListModelMixin,
+                              mixins.DestroyModelMixin,
+                              mixins.CreateModelMixin,
+                              viewsets.GenericViewSet):
+    pass
