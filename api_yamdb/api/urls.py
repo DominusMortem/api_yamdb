aac92810dd2fa1219278f382d12dc364a4369bd0
Алексей Уколов <ulex07@gmail.com>
2022-04-30 13:15:28 +0400

    Исправлена ошибка в urls

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -5,7 +5,7 @@ from .views import (CategoryViewSet, CommentViewSet,
 
 router = routers.DefaultRouter()
 router.register('categories', CategoryViewSet, basename='categories')
-router.register('genres', GenresViewSet, basename='categories')
+router.register('genres', GenresViewSet, basename='genres')
 router.register('titles', TitleViewSet, basename='titles')
 router.register(
     r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
