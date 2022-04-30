df106cada7d3deeab131f9108fd708520343ae70
Серафим Попов <80106081+DominusMortem@users.noreply.github.com>
2022-04-30 15:43:24 +0300

    Update permissions.py

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -3,10 +3,10 @@ from rest_framework import permissions
 
 class IsAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
-        return bool(request.user and
-                    request.user.is_authenticated and
-                    request.user.role == 'admin' or
-                    request.user.is_staff)
+        return (request.user and
+                request.user.is_authenticated and
+                request.user.role == 'admin' or
+                request.user.is_staff)
 
 
 class IsModerator(permissions.BasePermission):
